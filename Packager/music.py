7f188934a4bdefa888a7116dff441cce4ca96fbd
y17 <gaudincarrollldj@gmail.com>
2025-05-13 19:51:42 +0800

    修改随机抽取音乐文件

--- Packager/music.py
+++ Packager/music.py
@@ -6,8 +6,7 @@ def m():
     Path = "./Music"
     MusicList = os.listdir(Path)
     Num = len(MusicList)
-    # MusicList = ["Happy brithday to you.oga","……すごく嬉しい。ありがとう.oga",
-    #             "おかえり、先生.oga","この花、もしかして….oga","ブルーアーカイブ.oga"]
     Number = random.randint(0,Num-1)
     return MusicList[Number]
     #print(MusicList[Number])
+#m()
